fea816d2c30e40e28be008afd262e44d815a1e27
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-10-14 11:19:34 -0500

    fix settled_payroll validation on period.rb

--- app/models/period.rb
+++ app/models/period.rb
@@ -3,7 +3,7 @@ class Period < ApplicationRecord
   belongs_to :company
 
   validates :year, inclusion: {in: (2000..2022), message: "Only can generate a payroll beetwen 2000 and 2022.", code: "009"}
-  validate :same_year_and_month, :valid_month, :settled_payroll?
+  validate :same_year_and_month, :valid_month
 
   private
 
@@ -18,9 +18,9 @@ class Period < ApplicationRecord
     errors.add(:base, "Invalid month", code: "011") unless self.month in (1..12)
   end
 
-  def settled_payroll?
-    actual_period = Period.last
+  # def settled_payroll?
+  #   actual_period = Period.last
 
-    errors.add(:base, "you cannot create another period until you settle payroll for the current period", code: "013") if actual_period.payroll.nil?
-  end
+  #   errors.add(:base, "you cannot create another period until you settle payroll for the current period", code: "013") if actual_period && actual_period.payroll.nil?
+  # end
 end
