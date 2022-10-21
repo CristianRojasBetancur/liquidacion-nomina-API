44719ddc48f1cda52211a65c8a7c926f18065c45
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-10-20 22:12:59 -0500

    add gems required

---
+++ .history/config/routes_20221020220256.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  resources :modifications
+  resources :periods, only: [:index, :create, :show]
+  resources :payrolls
+  resources :workers, only: [:index, :create, :show]
+  resources :users
+  resources :companies, only: [:index, :create, :show]
+
+  # Auth route
+  post '/login', to: "auth#login"
+
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+end
---
+++ .history/config/routes_20221020221152.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  resources :modifications
+  resources :periods, only: [:index, :create, :show]
+  resources :payrolls
+  resources :workers, only: [:index, :create, :show]
+  resources :users
+  resources :companies, only: [:index, :create, :show]
+
+  # Auth route
+  post '/login', to: "auth#login"
+end
